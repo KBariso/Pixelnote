207acc1cbfeaa2f25d83c805d933266796a47824
kielbariso@gmail.com <kielbariso@gmail.com>
2022-01-06 12:43:52 -0600

    added Get Session User API Route

--- AuthenticateMe/backend/routes/api/session.js
+++ AuthenticateMe/backend/routes/api/session.js
@@ -30,6 +30,19 @@ router.post(
   })
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
