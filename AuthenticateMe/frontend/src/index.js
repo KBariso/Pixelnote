72d6ff433f419a33438ab887d639bfa303ebeb6f
kielbariso@gmail.com <kielbariso@gmail.com>
2022-01-06 15:25:44 -0600

    added custom csrfFetch, frontend setup complete

--- AuthenticateMe/frontend/src/index.js
+++ AuthenticateMe/frontend/src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 
-import configureStore from './store';
-
-
+import configureStore from "./store";
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
 }
 
@@ -30,5 +37,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
