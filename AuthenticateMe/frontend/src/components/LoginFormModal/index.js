fdff27806d5b1f000988aeacb6ec378459790afa
kielbariso@gmail.com <kielbariso@gmail.com>
2022-01-07 13:02:33 -0600

    made login form page modal:

---
+++ AuthenticateMe/frontend/src/components/LoginFormModal/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import LoginForm from "./LoginForm";
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>
+        Log In <i className="fas fa-sign-in-alt"></i>
+      </button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
