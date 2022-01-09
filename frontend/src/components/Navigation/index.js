cf8d769f10d9ce7f3dcd2f849674831478c94ddd
kielbariso@gmail.com <kielbariso@gmail.com>
2022-01-09 17:34:29 -0600

    fixed route for home and splash page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
+    sessionLinks = <ProfileButton user={sessionUser} />;
   } else {
     sessionLinks = (
       <>
+
+      <div className="right-nav">
+        <NavLink className="signup2" to="/signup">
+          Sign Up
+        </NavLink>
         <LoginFormModal />
-        <NavLink className='signup' to="/signup">Sign Up</NavLink>
+      </div>
+
       </>
     );
   }
 
   return (
-    <div className='navbar'>
-      <ul>
-        <li>
-          <NavLink className='home' exact to="/">Home</NavLink>
-          {isLoaded && sessionLinks}
-        </li>
-      </ul>
+    <div className="navbar">
+      <NavLink exact to="/home">
+        <div className="home">Pixelnote</div>
+      </NavLink>
+      {isLoaded && sessionLinks}
     </div>
-
   );
 }
 
---
+++ frontend/src/components/Splash/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navigation from "../Navigation";
+import "../../index.css";
+import "./splash.css";
+
+const Splash = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Splash;
